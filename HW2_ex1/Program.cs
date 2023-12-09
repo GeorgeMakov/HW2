e8dccbdadebf3fbd5b9ab0607c20b91dfd5c283e
George <gera.gora@yandex.rgit config ser.namegit config --global user.name Georgegit config user.namegit configgit config --global user.email gera.gora@yandex.r>
2023-12-09 13:35:18 +0300

    задача 1

---
+++ HW2_ex1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0)
+//    if (a % 23 == 0);
+    Console.WriteLine($"Ваше число {a} кратно 7 и 23");
+else
+{
+    Console.WriteLine($"Ваше число {a} не кратно 7 или 23");
+}
