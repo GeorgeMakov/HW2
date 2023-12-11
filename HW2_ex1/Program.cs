a39b55ec52d46f006bf316f674f0674bcddbd65d
GeorgeMakov <151999193+GeorgeMakov@users.noreply.github.com>
2023-12-11 19:52:58 +0300

    Update Program.cs

--- HW2_ex1/Program.cs
+++ HW2_ex1/Program.cs
@@ -1,9 +1,34 @@
-﻿Console.Write("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-if (a % 7 == 0 && a % 23 == 0)
-//    if (a % 23 == 0);
-    Console.WriteLine($"Ваше число {a} кратно 7 и 23");
-else
-{
-    Console.WriteLine($"Ваше число {a} не кратно 7 или 23");
-}
+// // Задача 1
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 7 == 0 && a % 23 == 0)
+// //    if (a % 23 == 0);
+//     Console.WriteLine($"Ваше число {a} кратно 7 и 23");
+// else
+// {
+//     Console.WriteLine($"Ваше число {a} не кратно 7 или 23");
+// }
+
+//using System;
+
+// Задача 2: Определение координатной четверти
+
+// Задача 3
+// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа
+// Console.Write("Введите первое число в интервале 10 - 99: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a < 99 & a > 10)
+//     Console.WriteLine($"Ваше число {a} в заданном интервале");
+// else
+// {
+//     Console.WriteLine($"Ваше число {a} не соответствует заданному интервалу");
+// }
+// int a1 = a / 10; 
+// int a2 = a - (a1*10);
+// Console.WriteLine($"a1={a1} и a2={a2}");
+// if (a1 >= a2)
+//     Console.WriteLine($"Число {a1} больше {a2}");
+// else
+// {
+//     Console.WriteLine($"Число {a2} больше {a1}");
+// }
